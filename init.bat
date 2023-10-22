8ff31518fb5da985d9578a82fec85bddfd237c61
Cyphall <cyphallbraus@gmail.com>
2023-10-22 23:56:08 +0200

    Updated CMakeLists.txt

--- init.bat
+++ init.bat
@@ -2,8 +2,12 @@
 set VCPKG_DEFAULT_TRIPLET=x64-windows-static
 git clone https://github.com/Microsoft/vcpkg
 cd vcpkg
+git checkout 830f86fb309ad7167468a433a890b7415fbb90a5
 call bootstrap-vcpkg.bat
 vcpkg install sdl2 glm
 rmdir /s /q .git
+rmdir /s /q buildtrees
+rmdir /s /q downloads
+rmdir /s /q packages
 cd ..
 pause
